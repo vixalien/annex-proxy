ecef8e625c1beef1dfbf5781c0ce723c27ae9593
Angelo Verlain <geoangercola@gmail.com>
2022-07-18 17:13:16 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# annex-proxy
+
+This is a simple proxy that proxies `https://extensions.gnome.org/` for [Annex](https://github.com/vixalien/annex)
+
+Start the app:
+
+```
+deno run -A mod.ts
+```
---
+++ mod.ts
@@ -0,0 +1,26 @@
+// https://medium.com/deno-the-complete-reference/deno-by-example-proxy-file-server-41ea7ae00ce6
+
+import { serve } from "https://deno.land/std@0.148.0/http/mod.ts";
+
+const API_URL = "https://extensions.gnome.org/";
+
+function copyHeader(headerName: string, to: Headers, from: Headers) {
+  const hdrVal = from.get(headerName);
+  if (hdrVal) {
+    to.set(headerName, hdrVal);
+  }
+}
+
+async function reqHandler(req: Request) {
+  const path = new URL(req.url).pathname;
+  const proxyRes = await fetch(API_URL + path);
+  const headers = new Headers();
+  copyHeader("content-length", headers, proxyRes.headers);
+  copyHeader("content-type", headers, proxyRes.headers);
+  copyHeader("content-disposition", headers, proxyRes.headers);
+  return new Response(proxyRes.body, {
+    status: proxyRes.status,
+    headers,
+  });
+}
+serve(reqHandler, { port: 8100 });
